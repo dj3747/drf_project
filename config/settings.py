50bc51de33db234a9caa8e914957409041119571
dj3747 <dj94911131@gmail.com>
2025-08-25 16:10:15 +0300

    add permissions.py

--- config/settings.py
+++ config/settings.py
@@ -47,10 +47,14 @@ ROOT_URLCONF = "config.urls"
 
 REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": [
-        "django_filters.rest_framework.DjangoFilterBackend"],
+        "django_filters.rest_framework.DjangoFilterBackend",
+    ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        "rest_framework.permissions.IsAuthenticated",
+    ]
 }
 
 TEMPLATES = [
