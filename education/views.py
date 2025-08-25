b15e24382b60ff6c60fd4120baee03f3b515fe18
dj3747 <dj94911131@gmail.com>
2025-08-25 19:14:05 +0300

    fixed education_views and urls

--- education/views.py
+++ education/views.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 
 from .models import Course, Lesson
 from .serializer import CourseSerializer, LessonSerializer
-from users.permissions import IsOwnerOrModerator, IsNotModerator, IsOwner
+from users.permissions import IsOwnerOrModerator, IsNotModerator, IsOwner, IsModerator
 
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
@@ -23,55 +23,62 @@ class CourseViewSet(viewsets.ModelViewSet):
         """Устанавливаем владельца при создании курса"""
         serializer.save(owner=self.request.user)
 
+    def get_queryset(self):
+        """Фильтруем курсы: модераторы видят все, остальные - только свои"""
+        user = self.request.user
+        if user.groups.filter(name='Модераторы').exists():
+            return Course.objects.all()
+        return Course.objects.filter(owner=user)
 
-class LessonCreateApiView(generics.CreateAPIView):
+
+class LessonCreateAPIView(generics.CreateAPIView):
     serializer_class = LessonSerializer
-    permission_classes = [IsAuthenticated, IsNotModerator]  # Только не-модераторы могут создавать
+    permission_classes = [IsAuthenticated, IsNotModerator]
 
     def perform_create(self, serializer):
         """Устанавливаем владельца при создании урока"""
         serializer.save(owner=self.request.user)
 
 
-class LessonListApiView(generics.ListAPIView):
+class LessonListAPIView(generics.ListAPIView):
     serializer_class = LessonSerializer
-    queryset = Lesson.objects.all()
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         """Фильтруем уроки: модераторы видят все, остальные - только свои"""
-        if self.request.user.groups.filter(name='Модераторы').exists():
+        user = self.request.user
+        if user.groups.filter(name='Модераторы').exists():
             return Lesson.objects.all()
-        return Lesson.objects.filter(owner=self.request.user)
+        return Lesson.objects.filter(owner=user)
 
 
-class LessonRetrieveApiView(generics.RetrieveAPIView):
+class LessonRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = LessonSerializer
-    queryset = Lesson.objects.all()
-    permission_classes = [IsAuthenticated, IsOwnerOrModerator]  # Владелец или модератор
+    permission_classes = [IsAuthenticated, IsOwnerOrModerator]
 
     def get_queryset(self):
         """Фильтруем уроки: модераторы видят все, остальные - только свои"""
-        if self.request.user.groups.filter(name='Модераторы').exists():
+        user = self.request.user
+        if user.groups.filter(name='Модераторы').exists():
             return Lesson.objects.all()
-        return Lesson.objects.filter(owner=self.request.user)
+        return Lesson.objects.filter(owner=user)
 
 
-class LessonUpdateApiView(generics.UpdateAPIView):
+class LessonUpdateAPIView(generics.UpdateAPIView):
     serializer_class = LessonSerializer
-    queryset = Lesson.objects.all()
-    permission_classes = [IsAuthenticated, IsOwnerOrModerator]  # Владелец или модератор
+    permission_classes = [IsAuthenticated, IsOwnerOrModerator]
 
     def get_queryset(self):
         """Фильтруем уроки: модераторы видят все, остальные - только свои"""
-        if self.request.user.groups.filter(name='Модераторы').exists():
+        user = self.request.user
+        if user.groups.filter(name='Модераторы').exists():
             return Lesson.objects.all()
-        return Lesson.objects.filter(owner=self.request.user)
+        return Lesson.objects.filter(owner=user)
 
 
-class LessonDestroyApiView(generics.DestroyAPIView):
-    queryset = Lesson.objects.all()
-    permission_classes = [IsAuthenticated, IsNotModerator, IsOwner]  # Только владелец и не модератор
+class LessonDestroyAPIView(generics.DestroyAPIView):
+    serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated, IsOwner, IsNotModerator]
 
     def get_queryset(self):
         """Фильтруем уроки: показываем только свои"""
